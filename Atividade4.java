a83529cbc9aadfeb87e9a05189bcf1bffbbd9f9b
Danilo <danilo88schmitt@gmail.com>
2026-05-20 21:15:21 -0300

    add os comentarios

--- Atividade4.java
+++ Atividade4.java
@@ -1,3 +1,15 @@
+/*
+Enunciado: Faça um programa para ler os valores de dois vetores de inteiros, cada um contendo 10 elementos. Crie um terceiro vetor em que cada elemento é a soma dos valores contidos nas posições respectivas dos vetores originais. Por exemplo, vetor1 = [1,2,3] vetor2 = [1,5,6] vetor3 = [2,7,9]. Exiba, ao final, os três vetores na tela.
+
+entrada: ler os valores de dois vetores de inteiros, cada um contendo 10 elementos
+
+processo: ler os valores e armazenar em dois vetores, criar um terceiro vetor para armazenar a soma dos valores dos dois vetores originais, depois imprimir os três vetores
+
+saida: os três vetores na tela
+*/
 public class Atividade4 {
     
 }
+/*
+testes
+*/
