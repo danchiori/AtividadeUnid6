d0d22635c502eab61adbba582accaa6fe75e342d
JoaoG <jgmdbusiness@hotmail.com>
2026-05-20 21:19:10 -0300

    abertura do Scanner e criação dos vetores

--- Atividade4.java
+++ Atividade4.java
@@ -1,4 +1,8 @@
-/*
+import java.util.Scanner;
+
+public class Atividade4 {
+
+    /*
 Enunciado: Faça um programa para ler os valores de dois vetores de inteiros, cada um contendo 10 elementos. Crie um terceiro vetor em que cada elemento é a soma dos valores contidos nas posições respectivas dos vetores originais. Por exemplo, vetor1 = [1,2,3] vetor2 = [1,5,6] vetor3 = [2,7,9]. Exiba, ao final, os três vetores na tela.
 
 entrada: ler os valores de dois vetores de inteiros, cada um contendo 10 elementos
@@ -7,8 +11,15 @@ processo: ler os valores e armazenar em dois vetores, criar um terceiro vetor pa
 
 saida: os três vetores na tela
 */
-public class Atividade4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int[] vetor1 = new int[10];
+        int[] vetor2 = new int[10];
+        int[] vetor3 = new int[10];
     
+    }
 }
 /*
 testes
